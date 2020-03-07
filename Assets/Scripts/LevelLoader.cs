9cae8eb31b0b5c7f82c1d785ce3dc9c8bc28fd2e
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2020-03-07 19:13:41 +0100

    Add Splash Screen and Start Screen

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 3f;
+
+    int currentSceneIndex;
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+           StartCoroutine(LoadStartScene());
+        }
+    }
+
+    private IEnumerator LoadStartScene()
+    {
+        yield return new WaitForSeconds(loadDelay);
+        LoadNextScene();
+
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex+1);
+    }
+}
