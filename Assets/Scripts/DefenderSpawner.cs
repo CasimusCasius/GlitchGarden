8cc8b971d12f35adb3dd24c38c4505abe60f1427
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2020-03-09 22:20:22 +0100

    Add Resources System, add Defender Buttons

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+
+    Defender defender;
+
+    private void OnMouseDown()
+    {
+        SpawnDefender(SnapToGrid(GetSquareClicked()));
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+    }
+
+    public void SetSelectedDefender(Defender defenderToSelect)
+    {
+        defender = defenderToSelect;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 position)
+    {
+        Defender newDefender = Instantiate(defender, position, Quaternion.identity) as Defender;
+    }
+}
---
+++ Assets/Scripts/Projectiles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour
+{
+    [SerializeField] float projectileSpeed=1f;
+    [SerializeField] int damagePoints = 50;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * projectileSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var health = collision.GetComponent<Health>();
+        var attacker = collision.GetComponent<Attacker>();
+
+        if (attacker && health)
+        {
+            health.DealDamage(damagePoints);
+            Destroy(gameObject);
+        }
+    }
+}
